211f27076546396543b1c637c78509bce417ba6f
Flion14 <104367621+Flion14@users.noreply.github.com>
2022-10-30 17:02:46 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -248,64 +248,64 @@ init();
 
 
 const canvas = document.getElementById('can1');
-const ctx = canvas.getContext('2d');
+const cts = canvas.getContext('2d');
 
 function draw(){
     
     
    
-  ctx.beginPath();
+  cts.beginPath();
  
-    ctx.moveTo(180,160);
-    ctx.lineTo(200,120);
-    ctx.lineTo(150,120);
-    ctx.lineTo(125,70);//ponta alta
-    ctx.lineTo(100,120);
-    ctx.lineTo(50,120);
-    ctx.lineTo(70,160);
-    ctx.lineTo(50,200);
-    ctx.lineTo(100,200);
-    ctx.lineTo(125,250);//ponta baixa
-    ctx.lineTo(150,200);
-    ctx.lineTo(200,200);
-    ctx.lineTo(180,160);
+    cts.moveTo(180,160);
+    cts.lineTo(200,120);
+    cts.lineTo(150,120);
+    cts.lineTo(125,70);//ponta alta
+    cts.lineTo(100,120);
+    cts.lineTo(50,120);
+    cts.lineTo(70,160);
+    cts.lineTo(50,200);
+    cts.lineTo(100,200);
+    cts.lineTo(125,250);//ponta baixa
+    cts.lineTo(150,200);
+    cts.lineTo(200,200);
+    cts.lineTo(180,160);
     
-  ctx.fillStyle='orange';
-    ctx.fill();
-  ctx.strokeStyle='black';
-    ctx.stroke();
+  cts.fillStyle='orange';
+    cts.fill();
+  cts.strokeStyle='black';
+    cts.stroke();
    
 } 
 function drawEye(){
-  ctx.beginPath();
-  ctx.moveTo(100, 145);
-ctx.arc(100, 145, 10, 0, 1*Math.PI, true);
-    ctx.moveTo(150, 145);
+  cts.beginPath();
+  cts.moveTo(100, 145);
+cts.arc(100, 145, 10, 0, 1*Math.PI, true);
+    cts.moveTo(150, 145);
 
 
-    ctx.arc(150, 145, 10, 0, 1*Math.PI, true);
-    ctx.moveTo(125, 185);
+    cts.arc(150, 145, 10, 0, 1*Math.PI, true);
+    cts.moveTo(125, 185);
     
-    ctx.arc(125, 185, 10, 0, 2*Math.PI, false);
+    cts.arc(125, 185, 10, 0, 2*Math.PI, false);
     
     
-    ctx.fillStyle='black';
-    ctx.fill();
-    ctx.strokeStyle='black';
-    ctx.stroke();
+    cts.fillStyle='black';
+    cts.fill();
+    cts.strokeStyle='black';
+    cts.stroke();
 }
 function drawPink(){
-  ctx.beginPath();
-ctx.arc(85, 165, 10, 0, 2*Math.PI, true);
-    ctx.moveTo(165, 165);
+  cts.beginPath();
+cts.arc(85, 165, 10, 0, 2*Math.PI, true);
+    cts.moveTo(165, 165);
 
 
-    ctx.arc(165, 165, 10, 0, 2*Math.PI, true);
+    cts.arc(165, 165, 10, 0, 2*Math.PI, true);
     
     
     
-    ctx.fillStyle='rgb(255, 204, 204)';
-    ctx.fill();
+    cts.fillStyle='rgb(255, 204, 204)';
+    cts.fill();
     
 }
 draw();
